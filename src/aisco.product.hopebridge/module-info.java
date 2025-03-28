8d2f2b4619436a18ccf48b2972b9f2702ccf6d51
DarilRaf <darilmrafansyah@gmail.com>
2025-03-28 23:50:55 +0700

    init and added new features

---
+++ src/aisco.product.hopebridge/module-info.java
@@ -0,0 +1,8 @@
+module aisco.product.hopebridge {
+    requires aisco.program.core;
+    requires aisco.program.operational;
+    requires aisco.financialreport.core;
+    requires aisco.financialreport.income;
+    requires aisco.financialreport.expense;
+    requires aisco.donation.core;
+}
